6f9833fb8b4886cbc52f611fc3b8064506f34027
westcripp <korukaltan@gmail.com>
2014-05-22 21:44:04 +0300

    Revert "Revert "fix duplicate LIBRARIES : build fix""
    This reverts commit b4650dee42d567603630a14ddb512b02913acea5.

--- Android.mk
+++ Android.mk
@@ -1,13 +1,23 @@
+#
+# Copyright (C) 2012 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := \
-    CameraWrapper.cpp
+ifeq ($(TARGET_DEVICE),i9300)
 
-LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils
+include $(call all-makefiles-under,$(LOCAL_PATH))
 
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_MODULE := camera.exynos4
-LOCAL_MODULE_TAGS := optional
-
+endif
